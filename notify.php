53f314b86bf7199efcee3032bc1cfb74dfaf11bb
ajmaurya101 <ajay@browserstack.com>
2020-07-07 01:10:47 +0530

    minor fixess

--- notify.php
+++ notify.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * Notify when Autoptimise cache file size reached 50%.
+ * Notify when [WordPress] Autoptimise cache file size reached 50% and 80%.
  */
 
 add_action(
   'admin_head',
   function () {
-    if (class_exists('autoptimizeCache')) {
+    if (class_exists('autoptimizeCacheNotify')) {
       // Retrieve the Autoptimize Cache Stats information.
-      $stats = autoptimizeCache::stats();
+      $stats = autoptimizeCacheNotify::stats();
 
       // Set the Max Size recommended for cache files.
       $max_size = apply_filters('autoptimize_filter_cachecheck_maxsize', 2 * 1024 * 1024 * 1024);
@@ -32,20 +32,20 @@ add_action(
           $datediff = intval(time() - $check_option);
           $days     = round($datediff / (60 * 60 * 24));
 
-          if ($percentage >= 80 && $days >= 1) { // send mail per day for more than 80%
+          if ($percentage >= 80 && $days >= 1) { // send mail once per day if cache size > 80%
             $send_mail = true;
-          } elseif ($days >= 7) { // send mail per week for more than 50%
+          } elseif ($days >= 7) { // send mail once per week if cache size > 50%
             $send_mail = true;
           }
         }
 
         if ($send_mail) {
-          $mailto      = 'user@example.com';
-          $mailsubject = 'Autoptimize Cache Size Increased';
-          $mailbody    = 'Curently the autoptimize\'s cache size is ' . $percentage . '% which is exceeding the default size of 50%, please delete the cache.';
+          $mail_to      = 'user@example.com';
+          $mail_subject = 'Autoptimize Cache Size Increased';
+          $mail_body    = 'Curently the autoptimize\'s cache size is ' . $percentage . '% which is exceeding the default size of 50%, please delete the cache.';
 
           // send mail and update time
-          wp_mail($mailto, $mailsubject, $mailbody);
+          wp_mail($mail_to, $mail_subject, $mail_body);
 
           // update current time
           update_option('autoptimize_cache_notify_mail', time());
